c9c2f9e045cbcdf5cf466b9efe3c2b4765022d1d
晨 方 <chenfang@bushinidiwifine.local>
2017-11-09 13:18:04 +0800

    fisrt commit

---
+++ data-structure/bst.cpp
@@ -0,0 +1,9 @@
+//
+//  bst.cpp
+//  data-structure
+//
+//  Created by 晨 方 on 2017/11/9.
+//  Copyright © 2017年 晨 方. All rights reserved.
+//
+
+#include "bst.hpp"
---
+++ data-structure/bst.hpp
@@ -0,0 +1,14 @@
+//
+//  bst.hpp
+//  data-structure
+//
+//  Created by 晨 方 on 2017/11/9.
+//  Copyright © 2017年 晨 方. All rights reserved.
+//
+
+#ifndef bst_hpp
+#define bst_hpp
+
+#include <stdio.h>
+
+#endif /* bst_hpp */
